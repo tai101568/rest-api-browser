18089569f9a25378eadee534142953e84c99ddc4
Dieu Nguyen <nhoangdieu@tma.com.vn>
2016-07-05 15:59:05 +0700

    show authorizations

--- swagger-ui.js
+++ swagger-ui.js
@@ -332,9 +332,14 @@ function program3(depth0,data) {
   }
 
 function program5(depth0,data) {
+  var buffer = "";
+    if(depth0.authorizations){
+        buffer += "\n        <h4>Permission</h4>\n        <p>";
+        buffer += depth0.authorizations.join("<br />");
+        buffer += "</p>\n        ";
+    }
   
-  
-  return "\n          <h4>Parameters</h4>\n          <table class='fullwidth'>\n          <thead>\n            <tr>\n            <th style=\"width: 100px; max-width: 100px\">Parameter</th>\n            <th style=\"width: 310px; max-width: 310px\">Value</th>\n            <th style=\"width: 200px; max-width: 200px\">Description</th>\n            <th style=\"width: 100px; max-width: 100px\">Parameter Type</th>\n            <th style=\"width: 220px; max-width: 230px\">Data Type</th>\n            </tr>\n          </thead>\n          <tbody class=\"operation-params\">\n\n          </tbody>\n          </table>\n          ";
+  return buffer+"\n          <h4>Parameters</h4>\n          <table class='fullwidth'>\n          <thead>\n            <tr>\n            <th style=\"width: 100px; max-width: 100px\">Parameter</th>\n            <th style=\"width: 310px; max-width: 310px\">Value</th>\n            <th style=\"width: 200px; max-width: 200px\">Description</th>\n            <th style=\"width: 100px; max-width: 100px\">Parameter Type</th>\n            <th style=\"width: 220px; max-width: 230px\">Data Type</th>\n            </tr>\n          </thead>\n          <tbody class=\"operation-params\">\n\n          </tbody>\n          </table>\n          ";
   }
 
 function program7(depth0,data) {
